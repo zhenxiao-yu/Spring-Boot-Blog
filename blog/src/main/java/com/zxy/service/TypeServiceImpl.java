597ae5829e2949a214e5fc7a9ddba477cfab112f
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-07-04 15:09:20 -0400

    Category Control Classes

--- blog/src/main/java/com/zxy/service/TypeServiceImpl.java
+++ blog/src/main/java/com/zxy/service/TypeServiceImpl.java
@@ -19,31 +19,47 @@ public class TypeServiceImpl implements TypeService{
     @Transactional
     @Override
     public Type saveType(Type type) {
+        //save current category
         return typeRepository.save(type);
     }
 
+    @Transactional
     @Override
     public Type getType(Long id) {
-        return null;
+        //retrieve category by id
+        return typeRepository.findOne(id);
     }
 
     @Override
     public Type getTypeByName(String name) {
-        return null;
+        //retrieve category by name
+        return typeRepository.findByName(name);
     }
 
+    @Transactional
     @Override
     public Page<Type> listType(Pageable pageable) {
-        return null;
+        //list of categories
+        return typeRepository.findAll(pageable);
     }
 
+    @Transactional
     @Override
     public Type updateType(Long id, Type type) {
-        return null;
+        //change category
+        Type t = typeRepository.findOne(id);
+        if (t == null) {
+            throw new NotFoundException("Category does not exist");
+        }
+        //pass the source date into the target data, performing update
+        BeanUtils.copyProperties(type,t);
+        return typeRepository.save(t);
     }
 
+    @Transactional
     @Override
     public void deleteType(Long id) {
-
+        //delete category
+        typeRepository.delete(id);
     }
 }

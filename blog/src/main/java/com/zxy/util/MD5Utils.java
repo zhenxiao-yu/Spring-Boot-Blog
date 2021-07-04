43166069fe5882fb51c73e08766dd8bb3ed78b7b
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-07-03 21:27:51 -0400

    Added MD5 code encryption service

--- blog/src/main/java/com/zxy/util/MD5Utils.java
+++ blog/src/main/java/com/zxy/util/MD5Utils.java
@@ -1,4 +1,38 @@
 package com.zxy.util;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class MD5Utils {
+
+    //encode user's password
+    public static String code(String str) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes());
+            byte[] byteDigest = md.digest();
+            int i;
+            StringBuffer buf = new StringBuffer("");
+            for (int offset = 0; offset < byteDigest.length; offset++) {
+                i = byteDigest[offset];
+                if (i < 0)
+                    i += 256;
+                if (i < 16)
+                    buf.append("0");
+                buf.append(Integer.toHexString(i));
+            }
+            //32 bit encrypt
+            return buf.toString();
+            //16 bit encrypt
+            //return buf.toString().substring(8, 24);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(code("615615"));
+    }
 }

d9956510fc6e4ea68a45c0e3d29753f5bf1b4d70
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-07-08 14:35:50 -0400

    Added comment controller

---
+++ blog/src/main/java/com/zxy/web/CommentController.java
@@ -0,0 +1,53 @@
+package com.zxy.web;
+
+import com.zxy.po.Comment;
+import com.zxy.po.User;
+import com.zxy.service.BlogService;
+import com.zxy.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
+
+
+@Controller
+public class CommentController {
+
+    //declare service classes
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private BlogService blogService;
+
+    @Value("${comment.avatar}")
+    private String avatar;
+
+    //comment on blog method
+    @GetMapping("/comments/{blogId}")
+    public String comments(@PathVariable Long blogId, Model model) {
+        model.addAttribute("comments", commentService.listCommentByBlogId(blogId));
+        return "blog :: commentList";
+    }
+
+    //publish comment method
+    @PostMapping("/comments")
+    public String post(Comment comment, HttpSession session) {
+        Long blogId = comment.getBlog().getId();
+        comment.setBlog(blogService.getBlog(blogId));
+        User user = (User) session.getAttribute("user");
+        if (user != null) {
+            comment.setAvatar(user.getAvatar());
+            comment.setAdminComment(true);
+        } else {
+            comment.setAvatar(avatar);
+        }
+        commentService.saveComment(comment);
+        return "redirect:/comments/" + blogId;
+    }
+}

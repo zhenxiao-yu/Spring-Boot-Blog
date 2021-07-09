1e057ece4bfd51b4155b6e1db6efff835bf5da9a
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-07-08 22:25:33 -0400

    tag update

--- blog/src/main/java/com/zxy/web/admin/TagController.java
+++ blog/src/main/java/com/zxy/web/admin/TagController.java
@@ -1,4 +1,5 @@
 package com.zxy.web.admin;
+//dependencies
 
 import com.zxy.po.Tag;
 import com.zxy.service.TagService;
@@ -18,7 +19,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.validation.Valid;
 
 
-
 @Controller
 @RequestMapping("/admin")
 public class TagController {
@@ -27,69 +27,74 @@ public class TagController {
     @Autowired
     private TagService tagService;
 
+    //direct to tags page
     @GetMapping("/tags")
-    public String tags(@PageableDefault(size = 10,sort = {"id"},direction = Sort.Direction.DESC)
-                                    Pageable pageable, Model model) {
-        model.addAttribute("page",tagService.listTag(pageable));
+    //divide into pages (10 items per page, sorted by id in reverse direction)
+    public String tags(@PageableDefault(size = 10, sort = {"id"}, direction = Sort.Direction.DESC)
+                               Pageable pageable, Model model) {
+        model.addAttribute("page", tagService.listTag(pageable));
         return "admin/tags";
     }
 
+    //add tag method
     @GetMapping("/tags/input")
     public String input(Model model) {
         model.addAttribute("tag", new Tag());
         return "admin/tags-input";
     }
 
+    //edit tag method
     @GetMapping("/tags/{id}/input")
     public String editInput(@PathVariable Long id, Model model) {
         model.addAttribute("tag", tagService.getTag(id));
         return "admin/tags-input";
     }
 
-
+    //task completion message controller
     @PostMapping("/tags")
-    public String post(@Valid Tag tag,BindingResult result, RedirectAttributes attributes) {
+    public String post(@Valid Tag tag, BindingResult result, RedirectAttributes attributes) {
+        //check if theres any duplicates
         Tag tag1 = tagService.getTagByName(tag.getName());
         if (tag1 != null) {
-            result.rejectValue("name","nameError","不能添加重复的分类");
+            result.rejectValue("name", "nameError", "Tag already exists!");
         }
         if (result.hasErrors()) {
             return "admin/tags-input";
         }
         Tag t = tagService.saveTag(tag);
-        if (t == null ) {
-            attributes.addFlashAttribute("message", "新增失败");
+        if (t == null) {
+            attributes.addFlashAttribute("message", "Unable to add new Tag!");
         } else {
-            attributes.addFlashAttribute("message", "新增成功");
+            attributes.addFlashAttribute("message", "New Tag added!");
         }
         return "redirect:/admin/tags";
     }
 
 
     @PostMapping("/tags/{id}")
-    public String editPost(@Valid Tag tag, BindingResult result,@PathVariable Long id, RedirectAttributes attributes) {
+    public String editPost(@Valid Tag tag, BindingResult result, @PathVariable Long id, RedirectAttributes attributes) {
+        //check if theres any duplicates
         Tag tag1 = tagService.getTagByName(tag.getName());
         if (tag1 != null) {
-            result.rejectValue("name","nameError","不能添加重复的分类");
+            result.rejectValue("name", "nameError", "Tag already exists!");
         }
         if (result.hasErrors()) {
             return "admin/tags-input";
         }
-        Tag t = tagService.updateTag(id,tag);
-        if (t == null ) {
-            attributes.addFlashAttribute("message", "更新失败");
+        Tag t = tagService.updateTag(id, tag);
+        if (t == null) {
+            attributes.addFlashAttribute("message", "Unable to update Tag!");
         } else {
-            attributes.addFlashAttribute("message", "更新成功");
+            attributes.addFlashAttribute("message", "Tag updated!");
         }
         return "redirect:/admin/tags";
     }
 
+    //delete tag method
     @GetMapping("/tags/{id}/delete")
-    public String delete(@PathVariable Long id,RedirectAttributes attributes) {
+    public String delete(@PathVariable Long id, RedirectAttributes attributes) {
         tagService.deleteTag(id);
-        attributes.addFlashAttribute("message", "删除成功");
+        attributes.addFlashAttribute("message", "Tag deleted!");
         return "redirect:/admin/tags";
     }
-
-
 }
--- blog/src/main/java/com/zxy/web/admin/TypeController.java
+++ blog/src/main/java/com/zxy/web/admin/TypeController.java
@@ -1,5 +1,5 @@
 package com.zxy.web.admin;
-
+//dependencies
 import com.zxy.po.Type;
 import com.zxy.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +27,7 @@ public class TypeController {
     @Autowired
     private TypeService typeService;
 
+    //direct to types page
     @GetMapping("/types")
     public String types(@PageableDefault(size = 10,sort = {"id"},direction = Sort.Direction.DESC)
                                     Pageable pageable, Model model) {
@@ -34,33 +35,35 @@ public class TypeController {
         return "admin/types";
     }
 
+    //add type method
     @GetMapping("/types/input")
     public String input(Model model) {
         model.addAttribute("type", new Type());
         return "admin/types-input";
     }
 
+    //edit type method
     @GetMapping("/types/{id}/input")
     public String editInput(@PathVariable Long id, Model model) {
         model.addAttribute("type", typeService.getType(id));
         return "admin/types-input";
     }
 
-
+    //task completion message controller
     @PostMapping("/types")
     public String post(@Valid Type type,BindingResult result, RedirectAttributes attributes) {
         Type type1 = typeService.getTypeByName(type.getName());
         if (type1 != null) {
-            result.rejectValue("name","nameError","不能添加重复的分类");
+            result.rejectValue("name","nameError","Category already exists!");
         }
         if (result.hasErrors()) {
             return "admin/types-input";
         }
         Type t = typeService.saveType(type);
         if (t == null ) {
-            attributes.addFlashAttribute("message", "新增失败");
+            attributes.addFlashAttribute("message", "Unable to add new Category!");
         } else {
-            attributes.addFlashAttribute("message", "新增成功");
+            attributes.addFlashAttribute("message", "New Category added!");
         }
         return "redirect:/admin/types";
     }
@@ -70,26 +73,25 @@ public class TypeController {
     public String editPost(@Valid Type type, BindingResult result,@PathVariable Long id, RedirectAttributes attributes) {
         Type type1 = typeService.getTypeByName(type.getName());
         if (type1 != null) {
-            result.rejectValue("name","nameError","不能添加重复的分类");
+            result.rejectValue("name","nameError","Category already exists!");
         }
         if (result.hasErrors()) {
             return "admin/types-input";
         }
         Type t = typeService.updateType(id,type);
         if (t == null ) {
-            attributes.addFlashAttribute("message", "更新失败");
+            attributes.addFlashAttribute("message", "Unable to update Category!");
         } else {
-            attributes.addFlashAttribute("message", "更新成功");
+            attributes.addFlashAttribute("message", "Category updated!");
         }
         return "redirect:/admin/types";
     }
 
+    //delete tag method
     @GetMapping("/types/{id}/delete")
     public String delete(@PathVariable Long id,RedirectAttributes attributes) {
         typeService.deleteType(id);
-        attributes.addFlashAttribute("message", "删除成功");
+        attributes.addFlashAttribute("message", "Category deleted!");
         return "redirect:/admin/types";
     }
-
-
 }
